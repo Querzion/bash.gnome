23972178d3e3ed3c5c625e88b4d5df900f359ddf
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-03 22:03:37 +0200

    Update bash.gnome-startup.sh

--- bash.gnome-startup.sh
+++ bash.gnome-startup.sh
@@ -18,19 +18,19 @@ detect_package_manager() {
 install_with_apt() {
     sudo apt update
     sudo apt upgrade -y
-    sudo apt install -y gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager python3 python3-psutil bluetooth bluez blueman git make
+    sudo apt install -y gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager
 }
 
 # Function to install packages using dnf
 install_with_dnf() {
     sudo dnf update -y
-    sudo dnf install -y gnome-tweaks gnome-extensions-app gnome-shell-extension-manager python3 python3-psutil bluetooth bluez blueman git make
+    sudo dnf install -y gnome-tweaks gnome-extensions-app gnome-shell-extension-manager
 }
 
 # Function to install packages using pacman
 install_with_pacman() {
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager python python-psutil bluez bluez-utils blueman git make
+    sudo pacman -S --noconfirm gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager
 }
 
 # Detect the package manager
@@ -53,25 +53,4 @@ case $PACKAGE_MANAGER in
         ;;
 esac
 
-# Install Autotiling
-git clone https://github.com/nwg-piotr/autotiling.git
-cd autotiling
-sudo ./install.sh
-cd ..
-rm -rf autotiling
-
-# Install Bluetooth battery indicator
-git clone https://github.com/Bleeptrack/gnome-shell-extension-bluetooth-quick-connect.git
-cd gnome-shell-extension-bluetooth-quick-connect
-make install
-cd ..
-rm -rf gnome-shell-extension-bluetooth-quick-connect
-
-# Enable Extensions
-gnome-extensions enable autotiling@nwg-piotr
-gnome-extensions enable bluetooth-quick-connect@bleeptrack
-
-# Inform the user about enabling extensions manually if needed
-echo "Gnome extensions installed. You may need to enable some of them manually through gnome-tweaks or gnome-shell-extension-manager."
-
-echo "Installation complete!"
+echo "Installation of GNOME Tweaks, Extensions, and Extension Manager complete!"
